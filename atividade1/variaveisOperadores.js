768769447ee3e3a2ea3c8f201e5d98392697b6af
jupitters <sampaiobale@gmail.com>
2025-03-07 08:51:45 -0300

    atividade de variaveis

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,18 @@
+let num_A = 10, num_B = 5;
+let resultado, operacao;
+
+operacao = "adição";
+resultado = num_A + num_B;
+console.log(`Resultado da ${operacao} entre ${num_A} e ${num_B} é ${resultado}`);
+
+operacao = "subtração";
+resultado = num_A - num_B;
+console.log(`Resultado da ${operacao} entre ${num_A} e ${num_B} é ${resultado}`);
+
+operacao = "multiplicação";
+resultado = num_A * num_B;
+console.log(`Resultado da ${operacao} entre ${num_A} e ${num_B} é ${resultado}`);
+
+operacao = "divisão";
+resultado = num_A / num_B;
+console.log(`Resultado da ${operacao} entre ${num_A} e ${num_B} é ${resultado}`);
