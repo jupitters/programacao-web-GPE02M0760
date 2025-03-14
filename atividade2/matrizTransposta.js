61c6fb5a5ac16dfba436e79f92fb3b50477b068a
jupitters <sampaiobale@gmail.com>
2025-03-14 09:25:20 -0300

    atividade 2 correção

--- atividade2/matrizTransposta.js
+++ atividade2/matrizTransposta.js
@@ -1,4 +1,21 @@
-function transporMatriz(A)
-{
-    
+function transporMatriz(A) {
+    console.log('Matriz\n');
+    for(let i = 0; i < A.length; i++) {
+        let linha = "";
+        for(let j = 0; j < A[i].length; j++) {
+            linha += A[i][j] + " ";
+        }
+        console.log(linha);
+    }
+
+    console.log('\nTransposta\n');
+    for(let j = 0; j < A[0].length; j++) {
+        let linha = "";
+        for(let i = 0; i < A.length; i++) {
+            linha += A[i][j] + " ";
+        }
+        console.log(linha);
+    }
 }
+
+transporMatriz([[1, 2], [3, 4], [5, 6]])
