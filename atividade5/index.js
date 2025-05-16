8a9a7f6844ed75163ce8864a23908d24bf4331b3
jupitters <sampaiobale@gmail.com>
2025-05-16 09:48:44 -0300

    form

--- atividade5/index.js
+++ atividade5/index.js
@@ -6,15 +6,34 @@ const PORT = 8080;
 app.engine('html', mustacheExpress());
 app.set('view-engine', 'html');
 app.set('views', __dirname + '/views');
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res)=>{
     res.render('index.html');
 })
 
-app.post('/agendar-consulta', (req, res)=> {
-    
+app.post('/agendar_consulta', (req, res)=> {
+    let data = req.body;
+    let erro_form = false;
+    let campos_invalidos = [];
+
+    if(data.nome.length == 0){
+        erro_form = true;
+        campos_invalidos.push("Nome")
+    }
+
+    if(data.sobrenome.length == 0){
+        erro_form = true;
+        campos_invalidos.push("Sobrenome")
+    }
+
+    if(data.cpf.length == 0){
+        erro_form = true;
+        campos_invalidos.push("CPF")
+    }
+
+
+    res.render('index.html', {erro_form, campos_invalidos, data});
 })
 
-app.listen(PORT, ()=>{
-    console.log(`Iniciando servidor na porta ${PORT}`);
-})
+app.listen(PORT, () => console.log(`Iniciando servidor na porta ${PORT}`))
